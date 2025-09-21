21f29247d7e06265a7d6e3b8513effa4da6a3826
VictorCosme <victorcosmemelo@gmail.com>
2025-09-20 22:16:31 -0300

    Preprocessa a cartilha para salvar localmente

---
+++ dataset/process_dataset.py
@@ -0,0 +1,13 @@
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+
+loader = PyPDFLoader('dataset/cartilha.pdf')
+docs = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+vectorstore = FAISS.from_documents(all_splits, embedding=embeddings)
+vectorstore.save_local("dataset/faiss_index")
